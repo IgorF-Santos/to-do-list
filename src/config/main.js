08dea76860f7ca7db6a4dce4493d635d9e771238
Igor Santos <keep@thinkeep-5260>
2022-06-11 14:30:00 -0300

    dark-mode

--- src/config/main.js
+++ src/config/main.js
@@ -1,6 +1,7 @@
-let btn = document.querySelector('#button-menu');
-let dashboardContainer = document.querySelector('.cards-view');
-let icon = document.querySelector('#menu-icon');
+const btn = document.querySelector('#button-menu');
+const dashboardContainer = document.querySelector('.cards-view');
+const icon = document.querySelector('#menu-icon');
+const darkMode = document.getElementById('dark-mode-button');
 
 btn.addEventListener('click', function(){
     if(dashboardContainer.style.display === 'block'){
@@ -12,6 +13,81 @@ btn.addEventListener('click', function(){
         icon.className = "fa-solid fa-circle-xmark";
     }
 });
+
+darkMode.addEventListener('click', function(){
+    const bodyElement = document.getElementById('body-container');
+    const cardsElements = document.getElementsByClassName('card');
+    const mainContainerElement = document.getElementById('main-container');
+    const taskInputElement = document.getElementById('task-input');
+    const taskMainContainerElement = document.getElementsByClassName('tasks-main-container');    
+    const textElements = document.getElementsByClassName('text');   
+    const darkModeIcon = darkMode.querySelector('.fa-solid')
+
+    if(darkModeIcon.className === 'fa-solid fa-sun'){
+        darkModeIcon.className = "fa-solid fa-moon";
+
+        darkModeIcon.style.color= '#E0FFFF';
+        
+        bodyElement.style.backgroundColor = '#16141e'
+        bodyElement.style.transition = 'background-color 2s';
+        mainContainerElement.style.backgroundColor = '#231f30';
+        mainContainerElement.style.transition = 'background-color 2s';
+
+        for(let i=0; i < textElements.length; i++){
+            //textElements[i].style.color = 'snow';
+            //textElements[i].style.transition = 'color 2s';
+        }
+
+        for(let i=0; i < cardsElements.length; i++){
+            cardsElements[i].style.backgroundColor = '#1b1728';
+            cardsElements[i].style.transition = 'background-color 2s';
+        }
+        
+        for(let i=0; i < taskMainContainerElement.length; i++){
+            taskMainContainerElement[i].style.backgroundColor = '#1b1728';
+            taskMainContainerElement[i].style.transition = 'background-color 2s';
+
+            taskMainContainerElement[i].addEventListener('mouseover', function() {
+                taskMainContainerElement[i].style.backgroundColor = '#242034'
+            });
+            taskMainContainerElement[i].addEventListener('mouseout', function() {
+                taskMainContainerElement[i].style.backgroundColor = '#1b1728'
+            });            
+        }
+        
+    }
+    else{
+        darkModeIcon.className = "fa-solid fa-sun";
+
+        darkModeIcon.style.color= '#FFFF00';
+        bodyElement.style.backgroundColor = '#604bc9'
+        bodyElement.style.transition = 'background-color 2s';
+        mainContainerElement.style.backgroundColor = '#4a35b0';
+        mainContainerElement.style.transition = 'background-color 2s';
+
+        for(let i=0; i < textElements.length; i++){
+            //textElements[i].style.color = '#1c1c1c';
+            //textElements[i].style.transition = 'color 2s';
+        }
+
+        for(let i=0; i < cardsElements.length; i++){
+            cardsElements[i].style.backgroundColor = '#533cc4';
+            cardsElements[i].style.transition = 'background-color 2s';
+        }
+        
+        for(let i=0; i < taskMainContainerElement.length; i++){
+            taskMainContainerElement[i].style.backgroundColor = '#533cc4';
+            taskMainContainerElement[i].style.transition = 'background-color 2s';
+
+            taskMainContainerElement[i].addEventListener('mouseover', function() {
+                taskMainContainerElement[i].style.backgroundColor = '#5b42cb'
+            });
+            taskMainContainerElement[i].addEventListener('mouseout', function() {
+                taskMainContainerElement[i].style.backgroundColor = '#533cc4'
+            });            
+        }
+    }
+})
 
 
 const form = document.getElementById('task-form');
@@ -32,7 +108,7 @@ form.onsubmit = function (e) {
 
 function addTask(description) {    
     if(description !== null && description !== undefined){                      
-        const mainContainer = document.createElement('div');
+        const taskMainContainerElement = document.createElement('div');
         const taskContainer = document.createElement('div');
         const newTask = document.createElement('input');
         const taskLabel = document.createElement('text-area');
@@ -46,31 +122,32 @@ function addTask(description) {
         //newTask.setAttribute('checked', false);
         taskLabel.setAttribute('value', description);
         taskLabel.setAttribute('readonly', true);
-        deleteButton.setAttribute('type', 'button');
+        deleteButton.setAttribute('type', 'button');        
         deleteIcon.classList.add("fa-solid", "fa-trash-can")        
 
         newTask.classList.add('check')     
-        taskLabel.classList.add('label')   
-        mainContainer.classList.add('main-container');
+        taskLabel.classList.add('label', 'text')   
+        
         taskContainer.classList.add('task-item');
         deleteButton.classList.add('delete-button')
-                        
-        mainContainer.appendChild(taskContainer);
+        taskMainContainerElement.classList.add('tasks-main-container')          
+        taskMainContainerElement.appendChild(taskContainer);
         taskContainer.appendChild(newTask);
         taskContainer.appendChild(taskLabel);
-        mainContainer.appendChild(deleteButton);
+        taskMainContainerElement.appendChild(deleteButton);
         deleteButton.appendChild(deleteIcon);
         taskLabel.appendChild(taskDescriptionNode);        
-        taskList.appendChild(mainContainer);
+        taskList.appendChild(taskMainContainerElement);
 
-        increaseTaskNumber();                
+        increaseTaskToDoNumber();   
+        increaseDoneTasksNumber();             
 
         deleteButton.addEventListener('click', () => {
             if(taskList.querySelector('#' + description)){
-                taskList.removeChild(mainContainer);
+                taskList.removeChild(taskMainContainerElement);
             }
             else if(done.querySelector('#' + description)){
-                done.removeChild(mainContainer);
+                done.removeChild(taskMainContainerElement);
             }
          
         })
@@ -80,12 +157,12 @@ function addTask(description) {
             if(!newTask.checked && (newTask.checked !== null || newTask.checked !== true || newTask.checked !== "checked" || newTask.checked !== undefined)){                
                 newTask.setAttribute('checked', true);
                 //console.log(newTask)
-                done.appendChild(mainContainer);
+                done.appendChild(taskMainContainerElement);
             }
             else if(done.querySelector('#' + description)){
                 newTask.removeAttribute('checked');
-                done.removeChild(mainContainer);
-                taskList.appendChild(mainContainer)
+                done.removeChild(taskMainContainerElement);
+                taskList.appendChild(taskMainContainerElement)
                 //console.log(newTask)
             }
             
@@ -125,7 +202,7 @@ function addTask(description) {
     //closeTask(description)
 }
 
-function increaseTaskNumber(){
+function increaseTaskToDoNumber(){
     const todo = document.querySelector('.list-number');
     const countCheckBox = taskList.getElementsByTagName('input').length;    
    
@@ -133,6 +210,17 @@ function increaseTaskNumber(){
     todo.appendChild(todoNode);
     todo.replaceChild(todoNode, todoNode);
     const vsf= document.getElementsByClassName('task-item').length
+    //console.log(vsf);
+}
+
+function increaseDoneTasksNumber(){
+    const todo = document.querySelector('.done-tasks-number');
+    const countCheckBox = done.getElementsByTagName('input').length;    
+   
+    const todoNode = document.createTextNode(countCheckBox);
+    todo.appendChild(todoNode);
+    //todo.replaceChild(todoNode, todoNode);
+    //const vsf= document.getElementsByClassName('task-item').length
     //console.log(vsf);
 }
 
